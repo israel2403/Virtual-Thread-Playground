b6b924dd48197d5eccd63b4a566fcbfc6ade12fa
Israel Huerta <36853507+israel2403@users.noreply.github.com>
2025-10-27 14:12:12 -0600

    Initial commit

---
+++ src/main/java/org/huerta/Main.java
@@ -0,0 +1,7 @@
+package org.huerta;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ src/main/java/org/huerta/sec01/InboundOutboundTaskDemo.java
@@ -0,0 +1,18 @@
+package org.huerta.sec01;
+
+public class InboundOutboundTaskDemo {
+
+    private static final int MAX_PLATFORM = 10;
+
+    public static void main(String[] args) {
+        platformThreadDemo();
+    }
+
+    private static void platformThreadDemo(){
+        for (int i = 0; i <  MAX_PLATFORM; i++) {
+            int j = i;
+            Thread thread = new Thread(()-> Task.ioIntensive(j));
+            thread.start();
+        }
+    }
+}
