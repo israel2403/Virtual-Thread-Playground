e4e9b73d4d5a98a876f4c493ee2daca16d5d8481
Israel Huerta <36853507+israel2403@users.noreply.github.com>
2025-10-28 12:19:08 -0600

    CountDownLatch is going to block the current app until all daemon threads die

--- src/main/java/org/huerta/sec01/InboundOutboundTaskDemo.java
+++ src/main/java/org/huerta/sec01/InboundOutboundTaskDemo.java
@@ -1,5 +1,7 @@
 package org.huerta.sec01;
 
+import java.util.concurrent.CountDownLatch;
+
 public class InboundOutboundTaskDemo {
 
     private static final int MAX_PLATFORM = 10;
@@ -25,12 +27,14 @@ public class InboundOutboundTaskDemo {
         }
     }
 
-    private static void platformThreadDemo3(){
+    private static void platformThreadDemo3() throws InterruptedException {
+        var latch = new CountDownLatch(MAX_PLATFORM);
         Thread.Builder.OfPlatform builder = Thread.ofPlatform().daemon().name("daemon", 1);
         for (int i = 0; i <  MAX_PLATFORM; i++) {
             int j = i;
             Thread thread = builder.unstarted(()-> Task.ioIntensive(j));
             thread.start();
         }
+        latch.await();
     }
 }
